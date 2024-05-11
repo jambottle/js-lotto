c5df0b547f7c70d7a8eecefe93d4466f61f76a2d
jambottle <jaewon.walks@gmail.com>
2024-05-11 20:50:11 +0900

    test: 로또 1장은 6개의 번호로 구성된다.

--- __test__/Lotto.test.js
+++ __test__/Lotto.test.js
@@ -6,3 +6,10 @@ describe('로또 가격은', () => {
     expect(Lotto.PRICE).toBe(1000);
   });
 });
+
+describe('로또 번호는', () => {
+  test('1장당 6개의 숫자로 구성된다.', () => {
+    // then: 로또 번호가 6개인지 확인
+    expect(Lotto.NUMBER_LENGTH).toBe(6);
+  });
+});
