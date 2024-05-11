f657c32c2aeb1137e28d1c59a217a2c5571a0160
jambottle <jaewon.walks@gmail.com>
2024-05-11 21:25:52 +0900

    feat: Lotto 클래스 > 인스턴스 생성 시, 예외 처리
    - 로또 번호의 개수가 6개가 아닌 경우, 예외 발생 처리
    - 1~45 사이의 정수가 아닌 숫자를 하나라도 허용할 경우, 예외 발생 처리

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,4 +1,25 @@
 export default class Lotto {
   static PRICE = 1000;
   static NUMBER_LENGTH = 6;
+
+  #numbers;
+
+  constructor(numbers) {
+    if (numbers.length !== Lotto.NUMBER_LENGTH) {
+      throw new Error('로또 1장은 6개의 번호로 구성되어야 합니다.');
+    }
+    if (!numbers.every(number => Lotto.isNumberValid(number))) {
+      throw new Error('로또 번호에는 1~45 사이의 정수만 부여할 수 있습니다.');
+    }
+    this.#numbers = numbers;
+  }
+
+  static isNumberValid(number) {
+    if (!Number.isInteger(number)) return false;
+    return 1 <= number && number <= 45;
+  }
+
+  get numbers() {
+    return this.#numbers;
+  }
 }
