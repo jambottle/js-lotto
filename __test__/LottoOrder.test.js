22f8cf0f8eff5280073ed6b4d5186f902ef38230
jambottle <jaewon.walks@gmail.com>
2024-05-16 16:00:35 +0900

    test: 로또 구입 금액은 1,000원 단위로 입력해야 한다.

---
+++ __test__/LottoOrder.test.js
@@ -0,0 +1,29 @@
+import LottoOrder from '../src/domain/LottoOrder.js';
+
+describe('로또 구입 금액을', () => {
+  test('최소 1,000원 이상 입력하지 않으면, 금액에 해당하는 만큼 로또를 주문할 수 없다.', () => {
+    // given: 최소 구입 금액을 충족하지 않는 금액 준비
+    const NOT_ENOUGH_AMOUNT = 500;
+
+    // when: 최소 구입 금액을 충족하지 않는 금액으로 로또 구입 시도
+    const orderLotto = () => new LottoOrder(NOT_ENOUGH_AMOUNT);
+
+    // then: 로또를 정상적으로 구매하였는지 확인
+    expect(orderLotto).toThrow(
+      '로또 구입 금액은 최소 1,000원 이상 입력되어야 합니다.'
+    );
+  });
+
+  test('1,000원 단위로 입력하지 않으면, 금액에 해당하는 만큼 로또를 주문할 수 없다.', () => {
+    // given: 1,000원 단위에 해당하지 않는 금액 준비
+    const NOT_EXACT_AMOUNT = 1100;
+
+    // when: 1,000원 단위에 해당하지 않는 금액으로 로또 구입 시도
+    const orderLotto = () => new LottoOrder(NOT_EXACT_AMOUNT);
+
+    // then: 로또를 정상적으로 구매하였는지 확인
+    expect(orderLotto).toThrow(
+      '로또 구입 금액은 1,000원 단위로 입력되어야 합니다.'
+    );
+  });
+});
