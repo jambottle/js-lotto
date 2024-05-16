cad8fef63849ae0dcfc1b2024acf3ca5c7a52602
jambottle <jaewon.walks@gmail.com>
2024-05-16 16:20:12 +0900

    feat: LottoOrder 클래스 > 인스턴스 생성 시, 예외 처리
    - 로또 구입 금액이 1,000원 단위가 아닌 경우, 예외 발생 처리
    - 최소 구입 금액에 대한 검사와 함께 별도의 메서드로 분리 (#validateAmount)

--- src/domain/LottoOrder.js
+++ src/domain/LottoOrder.js
@@ -8,9 +8,21 @@ export default class LottoOrder {
   #amount;
 
   constructor(amount) {
+    // 유효성 검사를 통과하면, #amount에 할당
+    this.#validateAmount(amount);
+    this.#amount = amount;
+  }
+
+  // 유효성 검사 method
+  #validateAmount(amount) {
+    if (!Number.isInteger(amount)) {
+      throw new Error('로또 구입 금액은 정수 타입으로 입력되어야 합니다.');
+    }
     if (amount < LottoOrder.MIN_AMOUNT) {
       throw new Error('로또 구입 금액은 최소 1,000원 이상 입력되어야 합니다.');
     }
-    this.#amount = amount;
+    if (amount % Lotto.PRICE !== 0) {
+      throw new Error('로또 구입 금액은 1,000원 단위로 입력되어야 합니다.');
+    }
   }
 }
