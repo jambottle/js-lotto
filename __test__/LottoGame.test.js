8e9b801dc2386df691742aec562608cd491b4b73
jambottle <jaewon.walks@gmail.com>
2024-05-18 20:05:16 +0900

    test: 로또 게임이 시작되고 나면, 당첨 기준에 따라 당첨금을 계산할 수 있다.
    - 당첨 기준에 따른 당첨금 값은 constants 폴더에서 별도 관리

--- __test__/LottoGame.test.js
+++ __test__/LottoGame.test.js
@@ -1,4 +1,6 @@
-import LottoGame from '../src/domain/LottoGame';
+import { LOTTO_PRIZE } from '../src/constants/LottoGame.constants.js';
+import Lotto from '../src/domain/Lotto.js';
+import LottoGame from '../src/domain/LottoGame.js';
 
 describe('로또 게임의', () => {
   describe('당첨 번호와 보너스 번호를 입력할 때,', () => {
@@ -82,6 +84,97 @@ describe('로또 게임의', () => {
       expect(createLottoGame).toThrow(
         '로또 게임의 보너스 번호는 당첨 번호와 중복될 수 없습니다.'
       );
+    });
+  });
+
+  describe('당첨 내역 및 수익률을 출력할 때,', () => {
+    let lottoGame;
+
+    beforeEach(() => {
+      const RANDOM_WINNING_NUMBERS = [1, 2, 3, 4, 5, 6];
+      const RANDOM_BONUS_NUMBER = 7;
+
+      lottoGame = new LottoGame(RANDOM_WINNING_NUMBERS, RANDOM_BONUS_NUMBER);
+    });
+
+    test('당첨 번호가 6개 모두 일치하는 경우, 2,000,000,000원의 당첨금을 지급한다.', () => {
+      // given: 당첨 번호가 6개 모두 일치하는 경우를 가정
+      jest.spyOn(lottoGame, 'getPointByWinningNumbers').mockReturnValue(6);
+
+      // when: 당첨금 계산
+      const randomLotto = new Lotto();
+      const randomLottoPrize = lottoGame.calculatePrize(randomLotto);
+
+      // then: 당첨금 계산 결과가 2,000,000,000원인지 확인
+      expect(LOTTO_PRIZE.FIRST).toBe(2000000000);
+      expect(randomLottoPrize).toBe(LOTTO_PRIZE.FIRST);
+    });
+
+    test('당첨 번호 5개와 보너스 번호까지 일치하는 경우, 30,000,000원의 당첨금을 지급한다.', () => {
+      // given: 당첨 번호 5개와 보너스 번호까지 일치하는 경우를 가정
+      jest.spyOn(lottoGame, 'getPointByWinningNumbers').mockReturnValue(5);
+      jest.spyOn(lottoGame, 'getIncludesBonusNumber').mockReturnValue(true);
+
+      // when: 당첨금 계산
+      const randomLotto = new Lotto();
+      const randomLottoPrize = lottoGame.calculatePrize(randomLotto);
+
+      // then: 당첨금 계산 결과가 30,000,000원인지 확인
+      expect(LOTTO_PRIZE.SECOND).toBe(30000000);
+      expect(randomLottoPrize).toBe(LOTTO_PRIZE.SECOND);
+    });
+
+    test('당첨 번호가 5개 일치하는 경우, 1,500,000원의 당첨금을 지급한다.', () => {
+      // given: 당첨 번호가 5개 일치하는 경우를 가정
+      jest.spyOn(lottoGame, 'getPointByWinningNumbers').mockReturnValue(5);
+      jest.spyOn(lottoGame, 'getIncludesBonusNumber').mockReturnValue(false);
+
+      // when: 당첨금 계산
+      const randomLotto = new Lotto();
+      const randomLottoPrize = lottoGame.calculatePrize(randomLotto);
+
+      // then: 당첨금 계산 결과가 1,500,000원인지 확인
+      expect(LOTTO_PRIZE.THIRD).toBe(1500000);
+      expect(randomLottoPrize).toBe(LOTTO_PRIZE.THIRD);
+    });
+
+    test('당첨 번호가 4개 일치하는 경우, 50,000원의 당첨금을 지급한다.', () => {
+      // given: 당첨 번호가 4개 일치하는 경우를 가정
+      jest.spyOn(lottoGame, 'getPointByWinningNumbers').mockReturnValue(4);
+
+      // when: 당첨금 계산
+      const randomLotto = new Lotto();
+      const randomLottoPrize = lottoGame.calculatePrize(randomLotto);
+
+      // then: 당첨금 계산 결과가 50,000원인지 확인
+      expect(LOTTO_PRIZE.FOURTH).toBe(50000);
+      expect(randomLottoPrize).toBe(LOTTO_PRIZE.FOURTH);
+    });
+
+    test('당첨 번호가 3개 일치하는 경우, 5,000원의 당첨금을 지급한다.', () => {
+      // given: 당첨 번호가 3개 일치하는 경우를 가정
+      jest.spyOn(lottoGame, 'getPointByWinningNumbers').mockReturnValue(3);
+
+      // when: 당첨금 계산
+      const randomLotto = new Lotto();
+      const randomLottoPrize = lottoGame.calculatePrize(randomLotto);
+
+      // then: 당첨금 계산 결과가 5,000원인지 확인
+      expect(LOTTO_PRIZE.FIFTH).toBe(5000);
+      expect(randomLottoPrize).toBe(LOTTO_PRIZE.FIFTH);
+    });
+
+    test('당첨 번호가 2개 이하로 일치하는 경우, 당첨금을 지급하지 않는다.', () => {
+      // given: 당첨 번호가 2개 이하로 일치하는 경우를 가정
+      jest.spyOn(lottoGame, 'getPointByWinningNumbers').mockReturnValue(2);
+
+      // when: 당첨금 계산
+      const randomLotto = new Lotto();
+      const randomLottoPrize = lottoGame.calculatePrize(randomLotto);
+
+      // then: 당첨금 계산 결과가 0원인지 확인
+      expect(LOTTO_PRIZE.NONE).toBe(0);
+      expect(randomLottoPrize).toBe(LOTTO_PRIZE.NONE);
     });
   });
 });
