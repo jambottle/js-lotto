8fae1b33f95769fb876e062eed4e59157836f9b5
jambottle <jaewon.walks@gmail.com>
2024-05-18 17:30:08 +0900

    test: 당첨 번호 6개와 보너스 번호 1개에는 1~45 사이의 정수만 부여할 수 있다.

--- __test__/LottoGame.test.js
+++ __test__/LottoGame.test.js
@@ -2,12 +2,12 @@ import LottoGame from '../src/domain/LottoGame';
 
 describe('로또 게임의', () => {
   describe('당첨 번호와 보너스 번호를 입력할 때,', () => {
-    test('당첨 번호 6개와 보너스 번호 1개가 중복되지 않으면, 게임을 시작할 수 있다.', () => {
-      // given: 중복되지 않는 당첨 번호 6개와 보너스 번호 1개 준비
+    test('당첨 번호 6개와 보너스 번호 1개에는 1~45 사이의 정수만 부여할 수 있다.', () => {
+      // given: 1~45 사이의 당첨 번호 6개와 보너스 번호 1개 준비
       const VALID_WINNING_NUMBERS = [1, 2, 3, 43, 44, 45];
       const VALID_BOUNS_NUMBER = 42;
 
-      // when: 중복되지 않는 당첨 번호 6개와 보너스 번호 1개로 로또 게임 생성
+      // when: 1~45 사이의 당첨 번호 6개와 보너스 번호 1개로 로또 게임 생성
       const lottoGame = new LottoGame(
         VALID_WINNING_NUMBERS,
         VALID_BOUNS_NUMBER
@@ -22,6 +22,36 @@ describe('로또 게임의', () => {
       // and: 로또 게임의 당첨번호와 보너스 번호가 정상적으로 할당되었는지 확인
       expect(lottoGame.winningNumbers).toEqual(VALID_WINNING_NUMBERS);
       expect(lottoGame.bonusNumber).toBe(VALID_BOUNS_NUMBER);
+    });
+
+    test('당첨 번호 6개에는 1~45 사이의 정수가 아닌 숫자를 하나라도 허용할 수 없다.', () => {
+      // given: 1~45 사이의 정수가 아닌 숫자를 포함하고 있는 당첨 번호의 배열 준비
+      const INVALID_WINNING_NUMBERS = [1, 2, 3, 43, 44, 46];
+      const RANDOM_BOUNS_NUMBER = 45;
+
+      // when: 1~45 사이의 정수가 아닌 숫자를 포함하고 있는 당첨 번호로 로또 게임 생성
+      const createLottoGame = () =>
+        new LottoGame(INVALID_WINNING_NUMBERS, RANDOM_BOUNS_NUMBER);
+
+      // then: 로또 게임이 정상적으로 생성되었는지 확인
+      expect(createLottoGame).toThrow(
+        '로또 게임의 당첨 번호에는 1~45 사이의 정수만 부여할 수 있습니다.'
+      );
+    });
+
+    test('보너스 번호에는 1~45 사이의 정수가 아닌 숫자를 허용할 수 없다.', () => {
+      // given: 1~45 사이의 정수가 아닌 숫자에 해당하는 보너스 번호 준비
+      const RANDOM_WINNING_NUMBERS = [1, 2, 3, 43, 44, 45];
+      const INVALID_BOUNS_NUMBER = 46;
+
+      // when: 1~45 사이의 정수가 아닌 숫자에 해당하는 보너스 번호로 로또 게임 생성
+      const createLottoGame = () =>
+        new LottoGame(RANDOM_WINNING_NUMBERS, INVALID_BOUNS_NUMBER);
+
+      // then: 로또 게임이 정상적으로 생성되었는지 확인
+      expect(createLottoGame).toThrow(
+        '로또 게임의 보너스 번호에는 1~45 사이의 정수만 부여할 수 있습니다.'
+      );
     });
 
     test('당첨 번호 6개에 중복된 번호가 포함되어 있으면, 게임을 시작할 수 없다.', () => {
